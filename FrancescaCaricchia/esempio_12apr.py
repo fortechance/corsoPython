d15c33a566d73bb7b8f580078cff9477f6e6e7e7
ForteChance <lahcenouhas@example.com>
2023-04-12 19:21:26 +0200

    prova

---
+++ FrancescaCaricchia/esempio_12apr.py
@@ -0,0 +1,9 @@
+nome = input("come ti chiami?")
+print(f"il tuo nome è {nome}")
+print(f"il tuo nome è {nome}")
+
+test = input("sei sicuro?")
+if (test=="no"):
+	nome = input("ripeti")
+else: 
+	pass
