e38f2a90bf503da42d0b3e88b4b2b52f59fac4d7
fortechance <manlio.lavalle@gmail.com>
2023-04-12 19:56:12 +0200

    .

---
+++ ManlioLaValle/esempio1.py
@@ -0,0 +1,27 @@
+import datetime
+nome = "Cattivix"
+orario = datetime.datetime.now()
+#obj = datetime.now()
+#orarioOK = obj.strftime("%HH-%mm-%ss")
+
+# richiamo la libreria datetime e lo chiamo pippo.datetime.now()
+#orario = pippo.datetime.now()  
+
+nome = input("come ti chiami?")
+test = input("sei sicuro?")
+if (test=="n"):
+	nome = input("Ripeti")
+else:
+	pass
+print("ciao,pippo")
+print(f"il più cattivo è {nome}")
+print(f"sono le {orario}")
+#print(f"in realtà sono le {orarioOK}")  #in questo caso uso  un unica stringa
+print("sono le:", orario)   #in questo caso uso la tupla
+
+
+#Domanda: posso richiamare la variabile puntualmente oppure con "f"
+#import datetime 
+#	oppure
+#from datetime import datetime
+
