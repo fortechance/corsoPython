6bfda74b24fac23520face5fd52bd62ec1b8d3be
forteChance <nicholas@mail.it>
2023-04-19 19:29:36 +0200

    Aggiornato file per lavorazione csv

--- nicholasLorenzini/provaCSV.py
+++ nicholasLorenzini/provaCSV.py
@@ -4,22 +4,30 @@ priKey = 0
 
 with open(nomeFile,"r") as f: #apro il file in modalità lettura (r = read, w=write. a=append)
     buffer = f.read() #apro il file
-    righe = buffer.split("\n") #split delle righe del csv alla fine della riga
-    print(righe)
-    print(len(righe)) #stampa quante righe mi ha tirato fuori
-    riga1 = len(righe[0])
-    dictDati = {}
+    righe = buffer.split("\n") #split delle righe del csv alla fine della riga \n
+    #print(righe[0]) #stampa Nome;Cognome;
+    #print(len(righe)) #stampa quante righe mi ha tirato fuori
+    dictDati = {} #inizializzo il dizionario
+    dictCampi = {}
     
-    for riga in righe:
+    for riga in righe: #per ogni riga dell'array
         if riga != "":#controllo che la mia riga abbia almeno 1 carattere | len(riga) > 1
+            dictCampi = {}
             colonne = riga.split(sep) #divido la riga per separatore
-            if len(colonne) != riga1:#il csv è formato da 4 colonne, se ce n'è di più non è valido
+            #print(f"colonne:{colonne[0]}, {colonne[1]}") #stampa Nome, Cognome....
+            print (len(colonne))
+            if len(colonne) != 4:#se le prime due colonne sono Nome, Cognome
                 print("separatore errato")
             else:
                 if (colonne[0]=="Cognome" and colonne[1]=="Nome"):
                     pass #non fare nulla, salta
                 else:
-                    priKey += 1
+                    priKey += 1 #incremento di 1 la primary key (id univoco)
                     print(f"id = {priKey} -> {colonne}")
+                    dictCampi["Nome"] = colonne[0]
+                    dictCampi["Cognome"] = colonne[1]
+                    dictCampi["Professione"] = colonne[2]
+                    dictCampi["Eta"] = colonne[3]
+                    dictDati["priKey"] = priKey #dictDati = {1:{"Nome":"Rossi", Cognome:Mario,...}}
         else:#se la riga è minore di 1 vuol dire che è vuota | riga vuota
             print("Nessun valore in questa riga")
