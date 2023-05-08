d8b3f3244bd37d2c7d35612bc4c8dbeaacc100a8
fortechance <129980938+fortechance@users.noreply.github.com>
2023-05-08 20:00:12 +0200

    Update telnet.py

--- LahcenOuhas/telnet.py
+++ LahcenOuhas/telnet.py
@@ -3,8 +3,8 @@ import telnetlib
 
 HOST = "192.168.1.205"
 user = input("Enter your Username: ")
-password = getpass.getpass()
-enablepassword = getpass.getpass()
+password = getpass.getpass()        
+enablepassword = getpass.getpass()  #password enable
 
 tn = telnetlib.Telnet(HOST)
 
@@ -14,9 +14,9 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
     
-tn.read_until(b"R1>")
-tn.write(b"enable\n")
-tn.write(enablepassword.encode('ascii') + b"\n")
+tn.read_until(b"R1>")   #name of router
+tn.write(b"enable\n")   #The first step in configuring a router is to be located at privileged mode
+tn.write(enablepassword.encode('ascii') + b"\n")  #enable password
 tn.write(b"conf t\n")
 tn.write(b"int loop 0\n")
 tn.write(b"ip add 192.168.2.205 255.255.255.0\n")
