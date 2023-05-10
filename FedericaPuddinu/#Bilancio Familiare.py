5a3a8cbc0c0d18bc62ffb4116dbb93e87bcf80d5
forteChance <silvia.colombatto@outlook.it>
2023-05-10 21:26:46 +0200

    aggiunta bilancio fam

---
+++ FedericaPuddinu/#Bilancio Familiare.py
@@ -0,0 +1,19 @@
+#Bilancio Familiare
+
+class Utente():
+    def __init__(self, nome, cognome,)
+        self.nome = nome
+        self.cognome = cognome
+
+
+
+class Tipowallwet():
+    def __init__ (self, descrizione)
+        
+        
+
+
+class Wallet():
+    def __init__(self, nomewallet t:Tipowallwet, descrizione)
+        self.nomewallet = nomewallet
+
