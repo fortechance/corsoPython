d96004a509a7cae70fa8303c41e4cc2681e67be7
fortechance <129980938+fortechance@users.noreply.github.com>
2023-05-12 22:57:23 +0200

    app.py

--- LahcenOuhas/Employee.py
+++ LahcenOuhas/Employee.py
@@ -1,8 +1,16 @@
 class Employee:
-    def __init__(self, name, age, department, manager):
+    def __init__(self, name, age, department, manager, rating):
         self.name = name
         self.age = age
         self. department = department
         self. manager = manager
+        self.rating = rating  #rating is from 1-5
 
+
+def is_excellent(self):
+    if self.rating >=4.5:
+        return True
+    
+    else:
+        return False
         
--- LahcenOuhas/app.py
+++ LahcenOuhas/app.py
@@ -1,8 +1,10 @@
 from Employee import Employee
-Employee1 =Employee("lahcen", 25,"fastcloud", True)
-Employee2 =Employee("islam", 26, "facebook", False)
+Employee1 =Employee("lahcen", 25,"fastcloud", True, 5)
+Employee2 =Employee("islam", 26, "facebook", False, 3.5)
 
-print(Employee1.name)
-print(Employee2.name)
-print(Employee1.age)
-print(Employee2.age)
+#print(Employee1.name)
+#print(Employee2.name)
+#print(Employee1.age)
+#print(Employee2.age)
+print(Employee1.rating)
+print(Employee2.rating)
