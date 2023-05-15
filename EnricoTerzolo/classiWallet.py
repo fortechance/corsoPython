316ad61c84c77e35484cc0d2005937dbf75d04f4
forteChance <enrico.terzolo@gmail.com>
2023-05-15 19:58:34 +0200

    proseguito esercizio Paola

--- EnricoTerzolo/classiWallet.py
+++ EnricoTerzolo/classiWallet.py
@@ -1,26 +1,61 @@
 class utente():
-    def __init__(self):
+    def __init__(self, cognome, nome, indirizzo, citta,procincia):
         pass
 
 class tipoWallet():
-    def __init__(self):
+    def __init__(self, descrizione, reale):
         pass
 
 class wallet():
-    def __init__(self, u:utente, tw:tipoWallet):
+    def __init__(self, u:utente, tw:tipoWallet, nome):
 
         self.movimenti = {}
         self.utente = u
         self.tipowallet = tw
+        self.walletName = nome
+        self.saldo = 0
+
         pass
 
 class causale():
+    def __init__(self, descrizione, segno):
+        self.descrizione = descrizione
+        self.segno = '+' if segno == '+' else '-'
+
+class causali():
+
     def __init__(self):
-        pass
+
+        self.elencoCausali = {}
+
+    def AddCausale(self, c:causale):
+
+        if self.CausaleExist(c.descrizione):
+            raise Exception('Causale Duplicata')
+        else:
+            self.elencoCausali[c.descrizione] = c
+    
+    def CausaleExist(self, descrizione):
+
+        if descrizione in self.elencoCausali.keys():
+            return True
+        else:
+            return False
+        
 class movimento():
-    def __init__(self,c:causale):
-        self.causale = c
-        pass
+    def __init__(self,c:causale, importo, elencoCau):
+        
+        if (c.descrizione in elencoCau.elencoCausali.keys()):
+
+            self.causale = c
+            self.importo = importo
+    
+            if (c.segno == '-'):
+                self.importo = self.importo * -1
+        else:
+            raise Exception(f'causale {c.descrizione} inesistente')
+
+        
 
 class bottino():
     def __init__(self):
@@ -28,4 +63,4 @@ class bottino():
         self.wallets = {}
 
         def addWallet(w:wallet):
-            wallets[w] = w
+            self.wallets[w.walletName] = w
