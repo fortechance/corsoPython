fceb2ec737104b088b862fcb74b467cfdbb6ef30
fortechance <129980938+fortechance@users.noreply.github.com>
2023-05-16 15:23:46 +0200

    causale.py

--- LahcenOuhas/test.py
+++ LahcenOuhas/test.py
@@ -1,12 +1,23 @@
-import json
+class Employee:
+    def __init__(self, name, age, department, manager, rating, salary):
+        self.name = name
+        self.age = age
+        self. department = department
+        self. manager = manager
+        self.rating = rating  #rating is from 1-5
+        self. salart= salary 
 
-# some JSON:
-x =  '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
-
-# the result is a Python dictionary:
-print(y["age"])
-print(y["name"])
-print(y["city"])
+def is_excellent(self):
+    if self.rating >=4.5:
+        return True
+    
+    else:
+        return False
+    
+    
+def bonus(self):
+    if self.age == 26:
+        self.salary += 500
+        print("salary increased to "+str(self.salary))
+    else:
+        print("no bonus added, salary is still " +str(self.salary))
---
+++ LahcenOuhas/utente.py
@@ -0,0 +1,7 @@
+from utente import*
+
+def __init_(self, nome, cognome, età):
+    self.nome = nome
+    self.cognome = cognome
+    self.età = età
+    print("nome")
