3c49f5aba2f663f519e5f43a813d8001c5821549
ForteChance <enricoterzolo@gmail.com>
2023-05-31 21:28:05 +0200

    aggiunto

---
+++ alessandromasini/sqlalchemy.py
@@ -0,0 +1,13 @@
+
+from urllib.parse import quote
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql://c73db:ocGB@QkcA8@python.hostingstudenti.fortechance.com/c73db')
+
+
+
+
+
+
+
+
