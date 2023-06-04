a1dae976b80c160ef499304ce16f761dc117651c
alessandro <ForteChance>
2023-06-04 22:41:38 +0200

    modificato sqlalchemy

--- alessandromasini/sqlalchemy.py
+++ alessandromasini/sqlalchemy.py
@@ -2,10 +2,16 @@
 from urllib.parse import quote
 from sqlalchemy import create_engine
 
+pw = 'ocGB@QkcA8'
+pwe = quote(pw,safe='')
+engine = create_engine(f'mysql://c73db:{pwe}@python.hostingstudenti.fortechance.com/c73db')
+
+
+
+
+'''
 engine = create_engine('mysql://c73db:ocGB@QkcA8@python.hostingstudenti.fortechance.com/c73db')
-
-
-
+'''
 
 
 
