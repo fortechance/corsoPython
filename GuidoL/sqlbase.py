8a5024a1bf81a61d649620421af89a79e83a5547
forteChance <enrico.terzolo@gmail.com>
2023-06-05 19:30:43 +0200

    varie db

--- GuidoL/sqlbase.py
+++ GuidoL/sqlbase.py
@@ -1,18 +1,14 @@
 from sqlalchemy import create_engine
 from urllib.parse import quote
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
 
 pw = "ocGB@QkcA8"
 pwe = quote(pw, safe="")
-
 # 89.40.173.164 - [192.168.10.16]
 host = "python.hostingstudenti.fortechance.com"
 sch = "c73db"
 us = "c73db"
-# password:ocGB@QkcA8
-
-engine = create_engine(f"mysql://{us}:{pwe}@{host}/{sch}")
-
-# engine = create_engine("mysql+mysqldb://scott:tiger@localhost/test")
 
 
 # 89.40.173.164 - [192.168.10.16]
@@ -20,3 +16,34 @@ engine = create_engine(f"mysql://{us}:{pwe}@{host}/{sch}")
 # schema:c73db
 # user:c73db
 # password:ocGB@QkcA8
+try:
+    # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
+    engine = create_engine(f"mysql://{us}:{pwe}@{host}/{sch}")
+
+    print(
+        f"Connection to the {host} for user {us} created successfully.")
+
+    m = MetaData()
+    
+    m.reflect(engine)
+    for table in m.tables.values():
+        print(table.name)
+        for column in table.c:
+            print(column.name)
+            
+
+    #students = Table(
+    #'students', m, 
+    #Column('id', Integer, primary_key = True), 
+    #Column('name', String(25)), 
+    #Column('lastname', String(25)), 
+    #)
+    #m.create_all(engine)
+    
+    
+    #students.drop(engine)
+
+    
+except Exception as ex:
+    print("Connection could not be made due to the following error: \n", ex)
+    
