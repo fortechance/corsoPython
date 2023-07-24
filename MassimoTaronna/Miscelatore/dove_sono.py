f55e15bb6f62db14676ee8a4eb4f0d00bd030219
ForteChance <massimo.taronna@gmail.com>
2023-07-24 18:34:30 +0200

    cancella dir

--- MassimoTaronna/Miscelatore/dove_sono.py
+++
@@ -1,4 +0,0 @@
-import os
-
-current_directory = os.getcwd()
-print("Directory corrente:", current_directory) 
--- MassimoTaronna/Miscelatore/prova_importa.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open("./dati.csv", newline="") as filecsv:
-    lettore = csv.reader(filecsv, delimiter=",")
-    header = next(lettore)
-    print (header)
